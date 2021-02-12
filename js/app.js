47834440d11475b3e56e2097ee8a08e5d08fd804
CodeScott <dougal_00@hotmail.com>
2021-02-12 16:09:48 +0000

    homework done

---
+++ js/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded')
+
+    const form = document.querySelector('#new-form'); //1
+    form.addEventListener('submit', handleFormInput)
+
+   
+   
+   
+    const shoppingList = document.querySelector('#deletebutton') //2
+    shoppingList.addEventListener('click', handleDeleteAll)
+
+})  
+
+
+const handleFormInput = function (event) {              //1
+    event.preventDefault()
+    console.log(event.target.expired.value)
+    const newElement = document.createElement('li')
+    newElement.textContent = `Food: ${event.target.title.value} Food Type: ${event.target.type.value} Expired? ${event.target.expired.value}`
+    const list = document.querySelector('#shoppingList')
+    list.appendChild(newElement)
+}
+
+
+
+const handleDeleteAll = function (event) {                 //2
+    event.preventDefault()
+    console.log(event)
+    const selectDelete = document.querySelector("#shoppingList");
+    selectDelete.textContent = ""; 
+
+}
+
